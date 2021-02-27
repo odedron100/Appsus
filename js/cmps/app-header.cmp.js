e602d621a2246266ccb1349cd01f6110d1418de3
Rom Soloman <romsoloman19@gmail.com>
2021-02-27 20:56:22 +0200

    fix img

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -11,19 +11,19 @@ export default {
            <svg @click="openNav" class="places" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
            <div v-if="isOpen" class="nav-open">
            <div class="nav-item" @click="openNav">
-                <img src="../../../img/home-icon.png" />
+                <img src="../../img/home-icon.png" />
                 <router-link active-class="active-link" to="/" exact>Home</router-link>
            </div>
            <div class="nav-item" @click="openNav">
-                <img src="../../../img/mail.jpeg" />
+                <img src="../../img/mail.jpeg" />
                 <router-link to="/email/inbox">Email</router-link> 
            </div>
            <div class="nav-item" @click="openNav">
-                <img src="../../../img/keep.png" />
+                <img src="../../img/keep.png" />
                 <router-link to="/keep">Keep</router-link>
            </div>
            <div class="nav-item" @click="openNav">
-                <img src="../../../img/book.png" />
+                <img src="../../img/book.png" />
                 <router-link to="/book">Book</router-link>
            </div>
            </div>
